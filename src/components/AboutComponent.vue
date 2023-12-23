81610d842b0a0e6dec3d99804bbf0dc2c57be2a1
Skuddie-Lungelo <139389336+Skuddie-Lungelo@users.noreply.github.com>
2023-12-23 18:58:10 +0200

    changes done
    ready

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="edrea_tm_section" id="about">
+    <div class="section_inner">
+      <div class="edrea_tm_about">
+        <div class="left">
+          <div class="image">
+            <img src="img/thumbs/1-1.jpg" alt="" />
+            <div class="main" data-img-url="img/about/1.jpg"></div>
+          </div>
+        </div>
+        <div class="right">
+          <div class="short">
+            <h3 class="name">Supremacy <span class="coloring">Culture</span></h3>
+            <h3 class="job">
+              <AnimationText />
+            </h3>
+          </div>
+          <div class="text">
+            <p>
+              Welcome to <span>Supremacy Culture Fx.</span> Our community believes that <span class="coloring">Trading</span>
+              not just a hobby or a casual pursuit, instead it is a serious business tha demands the right knowledge,skillset 
+              and strategy. We are here to guide you on your journey from a novice trader to a professional trader, running your Trading
+              endevavors as a thriving business.
+            </p>
+          </div>
+          <div class="edrea_tm_button" id="aboutPopup">
+            <a href="#">Start Business</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AnimationText from "./AnimationText.vue";
+export default {
+  name: `AboutComponent`,
+  data() {
+    return {
+      open: false,
+    };
+  },
+  components: {
+    AnimationText,
+  },
+};
+</script>
+
+<style scoped>
+.edrea_tm_modalbox {
+  top: -90px;
+}
+</style>
---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="edrea_tm_section" id="contact">
+    <div class="section_inner">
+      <div class="edrea_tm_contact">
+        <div class="edrea_tm_main_title">
+          <h3>Get in <span class="coloring">Touch</span></h3>
+        </div>
+        <div class="wrapper">
+          <div class="left">
+            <ul>
+              <li>
+                <div class="list_inner">
+                  <i class="icon-location"></i>
+                  <span
+                    ><a href="#" class="href_location"
+                      >Melville str. 7, PMB, S.A</a
+                    ></span
+                  >
+                </div>
+              </li>
+              <li>
+                <div class="list_inner">
+                  <i class="icon-phone"></i>
+                  <span><a href="#">+27 79 356 7998</a></span>
+                </div>
+              </li>
+              <li>
+                <div class="list_inner">
+                  <i class="icon-mail-1"></i>
+                  <span
+                    ><a href="mailto:Supremacyculturefx"
+                      >Supremacyculturefx@gmail.com</a
+                    ></span
+                  >
+                </div>
+              </li>
+              <li>
+                <div class="list_inner">
+                  <i class="icon-facebook-rect"></i>
+                  <span
+                    ><a href="https://web.facebook.com/profile.php?id=100054544310714">Supremacyculturefx</a></span
+                  >
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="right">
+            <div class="fields">
+              <form
+                action="/"
+                method="post"
+                class="contact_form"
+                id="contact_form"
+              >
+                <div
+                  class="returnmessage"
+                  data-success="Your message has been received, We will contact you soon."
+                ></div>
+                <div class="empty_notice">
+                  <span>Please Fill Required Fields</span>
+                </div>
+                <div class="first">
+                  <ul>
+                    <li>
+                      <div class="list_inner">
+                        <input
+                          id="name"
+                          type="text"
+                          placeholder="Name"
+                          autocomplete="off"
+                        />
+                      </div>
+                    </li>
+                    <li>
+                      <div class="list_inner">
+                        <input
+                          id="email"
+                          type="text"
+                          placeholder="Email"
+                          autocomplete="off"
+                        />
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div class="last">
+                  <textarea id="message" placeholder="Message"></textarea>
+                </div>
+                <div class="edrea_tm_button">
+                  <a id="send_message" href="#">Send Message</a>
+                </div>
+
+                <!-- If you want change mail address to yours, just open "modal" folder >> contact.php and go to line 4 and change detail to yours.  -->
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `ContactComponent`,
+};
+</script>
